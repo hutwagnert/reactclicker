e01d91b93e9d469b120728214f05904589d3e7fa
Tate Hutwagner <thutwagner@gmail.com>
2019-02-19 14:25:27 -0500

    css and love

--- myapp/src/components/Card/Card.js
+++ myapp/src/components/Card/Card.js
@@ -4,7 +4,8 @@ import "./Card.css";
 const Card = props => (
   <div className="card" onClick={() => props.clickCount(props.id)}>
     <div className="img-container">
-      <img alt={props.name} src={props.image} />
+    <span>{props.name}</span>
+      <img  className="imgimg" alt={props.name} src={props.image} />
     </div>
   </div>
 );
